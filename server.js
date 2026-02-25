4a1f8dbf629d7f7e15a36f125e416691febb7d8d
Mark <mark.davidovich@gmail.com>
2026-02-25 18:41:19 +0200

    add exercise 2: a dynamic route that checks word count

--- server.js
+++ server.js
@@ -8,9 +8,22 @@ const PORT = process.env.PORT || 8080;
 app.use(cors());
 app.use(express.json());
 
+const wordCounter = {};
+
 //EX1:
 app.get("/sanity", (req, res) => {
   res.send("Server is up and running");
+});
+
+//EX2:
+app.get("/:word", (req, res) => {
+  const word = req.params.word;
+
+  if (wordCounter[word]) {
+    res.json({ count: wordCounter[word] });
+  }
+
+  res.json({ count: 0 });
 });
 
 app.listen(PORT, () => {
